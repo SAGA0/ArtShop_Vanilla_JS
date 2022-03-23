c5a727d64973e770da8eb164b4a4a2b4121da32d
Ramz <murdagaliev95@gmail.com>
2022-03-23 15:00:44 +0300

    added filter and change animation

---
+++ src/js/modules/changeSize.js
@@ -0,0 +1,32 @@
+const changeSize = (imgBlock) => {
+	const blocks = document.querySelectorAll(imgBlock)
+
+	function showImg(block) {
+		const img = block.querySelector('img')
+
+		img.src = img.src.slice(0, -4) + '-1.png'
+		block.querySelectorAll('p:not(.sizes-hit)').forEach((item) => {
+			item.style.display = 'none'
+		})
+	}
+
+	function hideImg(block) {
+		const img = block.querySelector('img')
+
+		img.src = img.src.slice(0, -6) + '.png'
+		block.querySelectorAll('p:not(.sizes-hit)').forEach((item) => {
+			item.style.display = 'block'
+		})
+	}
+
+	blocks.forEach((block) => {
+		block.addEventListener('mouseover', () => {
+			showImg(block)
+		})
+		block.addEventListener('mouseout', () => {
+			hideImg(block)
+		})
+	})
+}
+
+export default changeSize
